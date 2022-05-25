ab8b28116a2046754a971ab94e061288cc72f6c9
Riches <richeskelechi94.rk@gmail.com>
2022-05-25 16:33:52 +0100

    Modified the algorithm

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Read text from a file, and count the occurence of words in that text
 # Example:
 # count_words("The cake is done. It is a big cake!")
-# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
+# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1, "done":1}
 
 def read_file_content(filename):
     # here I am trying to open the file.
